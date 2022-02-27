821ced8a376760241797fa3e8e1a7e0eedf01a20
Grace Wang <graceannwang@gmail.com>
2022-02-26 22:49:16 -0600

    added playRound()

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+/* computerPlay
+OUT: (String) what the computer plays */
 function computerPlay(){
-    let n = Math.floor(Math.random() * 10);
+    const n = Math.floor(Math.random() * 10);
     if(n >= 7){
         return "rock";
     } else if(n >= 4){
@@ -9,10 +11,30 @@ function computerPlay(){
     }
 }
 
-console.log(computerPlay());
+/* playRound
+IN: 
+    playerSelection: (String) the player's selection
+    computerSelection: (String) the computer's selection
+OUT: (String) description of who wins, who loses */
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
 
-/*
-Generate a random integer between 1 - 9
-If the number is 7 or greater, return rock
-if the number is 4 or greater, return paper
-if the number is 1 or greater, return scissors */
+    const resultStr = "computer guessed " + computerSelection + " --> ";
+
+    if(playerSelection == computerSelection) {
+        return resultStr + "tie";
+    }
+
+    const playerLoses = ((playerSelection == "rock") && (computerSelection == "paper")) ||
+                        ((playerSelection == "paper") && (computerSelection == "scissors")) ||
+                        ((playerSelection == "scissors") && (computerSelection == "rock"));
+    
+    if(playerLoses) {
+        return resultStr + "player loses, computer wins";
+    }
+
+    return resultStr + "player wins, computer loses";
+}
+
+console.log(playRound("scissors", computerPlay()));
